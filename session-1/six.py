b7e52bebcaa3c7575f062180b0cb179e8a50d769
Nishanth-gnn <gnishanth2005@gmail.com>
2025-02-05 22:14:12 +0530

    Create six.py

---
+++ session-1/six.py
@@ -0,0 +1,14 @@
+def add_to_dict(d, key, value):
+    d[key] = value
+
+my_dict = {}
+
+# Adding five key-value pairs
+add_to_dict(my_dict, 'name', 'Alice')
+add_to_dict(my_dict, 'age', 30)
+add_to_dict(my_dict, 'city', 'New York')
+add_to_dict(my_dict, 'job', 'Engineer')
+add_to_dict(my_dict, 'hobby', 'Reading')
+
+print("Dictionary:", my_dict)
+
